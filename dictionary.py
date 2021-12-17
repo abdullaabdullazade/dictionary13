05d5d2f947c62cecf97a62e40ee8fb4be2074cc5
okaberkarabin <89068450+okaberkarabin@users.noreply.github.com>
2021-12-17 05:33:23 -0700

    Update dictionary.py

--- dictionary.py
+++ dictionary.py
@@ -12,7 +12,7 @@ async def on_ready():
 
 @Bot.event
 async def on_message(msg):
-    with open("Botun lügət sözləri.txt", "r") as a:
+    with open("Botun lügət sözləri.txt", "r",encoding="utf-8") as a:
         for i in a:
             if i[:len(msg)].lower() == msg.content.lower():
                 await msg.author.send(i)
