ce3b9d78ac9c05c05a8be36e723532e5e4dca325
okaberkarabin <89068450+okaberkarabin@users.noreply.github.com>
2021-12-18 18:31:40 +0400

    Update dictionary.py

--- dictionary.py
+++ dictionary.py
@@ -16,5 +16,6 @@ async def on_message(msg):
    for i in a:
             if msg.content.lower() in i.lower():
                 await msg.author.send(i)
+                break
                 
 Bot.run('OTIxNzY3MjQ2MTk4NjE2MTI0.Yb3spQ.HQGToTS-KPJrK8qubT64O0KhKAw')
